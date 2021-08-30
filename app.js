8b7b76063c134c9a08991332f2dbcfa7f2d6524c
Sahil Duhan <duhan.sahil16@gmail.com>
2021-08-31 01:33:11 +0530

    git dupaet

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const prodctRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
+app.use(morgan('dev'));
 app.use('/products', prodctRoutes);
 app.use('/orders', orderRoutes);
+app.use((req, res, next) => {
+    const error = new Error('Error 404, Not Found')
+    error.status = 404;
+    next(error);
+});
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
 module.exports = app;
