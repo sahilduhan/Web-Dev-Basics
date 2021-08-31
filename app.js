091b07f0ea6c6677b29ac1486a8a142284da4474
Sahil Duhan <duhan.sahil16@gmail.com>
2021-09-01 00:02:32 +0530

    git update

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const app = express();
 const morgan = require('morgan');
 const prodctRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
+const bodyParser = require('body-parser')
 app.use(morgan('dev'));
 app.use('/products', prodctRoutes);
 app.use('/orders', orderRoutes);
+app.use(bodyParser.urlencoded({ extension: false }));
+app.use(bodyParser.json());
 app.use((req, res, next) => {
     const error = new Error('Error 404, Not Found')
     error.status = 404;
